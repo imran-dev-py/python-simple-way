d9774a2173c474cca68646e65b1b353017d44d8a
Imran Rashid <75940593+imran-rashid@users.noreply.github.com>
2021-06-04 11:57:33 +0600

    Update List.py

--- python-language-fundamentals/List.py
+++ python-language-fundamentals/List.py
@@ -7,7 +7,7 @@
 # 3) list is represented by []
 # 4) list is mutable
 # 5) growable in native [in place]
-# 6) support heterogeneous objects
+# 6) support heterogeneous(different type) objects
 # 7) positive and negative indexing are possible
 
 l = list()
@@ -19,4 +19,4 @@ print(l) # [10, 20, 30, 40]
 l.remove(30)
 print(l) # [10, 20, 40]
 
-print(__doc__) # None [""" """ strings represent as doc string]
+print(__doc__) # None 

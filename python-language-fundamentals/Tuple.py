19f188b01c143819a86f6bc78fff8f224244785e
Imran Rashid <75940593+imran-rashid@users.noreply.github.com>
2021-06-04 12:09:55 +0600

    Update Tuple.py

--- python-language-fundamentals/Tuple.py
+++ python-language-fundamentals/Tuple.py
@@ -1,12 +1,12 @@
 # If we want to represent a collection of values in a single entity, where duplicates are allowed and order is
-# preserved and values are immutable / read only then we can use tuple
+# preserved and values are immutable/read only then we can use tuple
 
-# characteristic's of list:=
+# characteristic's of tuple:=
 # 1) order is preserved
 # 2) duplicates are allowed
 # 3) tuple is represented by ()
 # 4) tuple is immutable
-# 6) support heterogeneous objects
+# 6) support heterogeneous(different type) objects
 # 7) positive and negative indexing are possible
 
 l = tuple((10, 20, 30, 40))

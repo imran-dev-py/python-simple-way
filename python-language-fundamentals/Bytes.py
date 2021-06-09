8953d7494d3c982c31654b422a48edb91f5cb161
Imran Rashid <75940593+imran-rashid@users.noreply.github.com>
2021-06-09 14:20:04 +0600

    Update Bytes.py

--- python-language-fundamentals/Bytes.py
+++ python-language-fundamentals/Bytes.py
@@ -1,19 +1,21 @@
 # bytes data types are useful when deal with binary data [images, files]
-# bytes value range is 0 to 255, more than 255 is ValueError
+# bytes value range is range(0, 256) [up to & not including], more than 255 is ValueError
 # immutable
+# iterable
 
 l = list(range(10, 50, 10))
 b = bytes(l)
-print(b)
+print(b) # b'\n\x14\x1e('
 print(type(b)) # bytes
 print(b[0]) # 10
 print(b[2:]) # b'\x1e('
 
-[print(x) for x in b]
+>>> [x for x in b] # [10, 20, 30, 40]
 
-# byteArray
-# byteArray value range is 0 to 255, more than 255 is ValueError
+# ========================================== byteArray() ===============================
+# byteArray value range is range(0, 256), more than 255 is ValueError
 # mutable
+# iterable, sliceable, indexing allowed
 
 ll = list(range(10, 50, 10))
 b = bytearray(ll)
@@ -22,4 +24,4 @@ print(b)
 print(b[-1])
 print(b[2:])
 b[0] = 00
-[print(x) for x in b]
+>>> [x for x in b] # [10, 20, 30, 40]

25e40bbe9cbbe316898d22c3bcd73c85bce2e2ef
Imran Rashid <75940593+imran-rashid@users.noreply.github.com>
2021-06-09 13:59:03 +0600

    Update Range.py

--- python-language-fundamentals/Range.py
+++ python-language-fundamentals/Range.py
@@ -8,8 +8,8 @@ for i in range(5):
     print(i, end='|') # 0|1|2|3|4
 print()
 
-r = range(20, 5, -5)
-rr = range(5, 20, 5) # range(5, 20, 5) 
+r = range(20, 5, -5) # for -ve step BEGIN value must be > than END value else IndexError: out of range
+rr = range(5, 20, 5) # range(5, 20, 5) for +ve step END value must be > than BEGIN value else IndexError: out of range
 print(rr[0:2]) # range(5, 15, 5) (up to 15)
 print(r[0:2]) # range(20, 10, -5) [10 is ending point & it's not included]
 print(r[0]) # 20

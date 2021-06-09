f861cefa9d78ff2e595cdfeebc5d4ba1c94640c2
Imran Rashid <75940593+imran-rashid@users.noreply.github.com>
2021-06-09 13:54:43 +0600

    Update Range.py

--- python-language-fundamentals/Range.py
+++ python-language-fundamentals/Range.py
@@ -5,11 +5,12 @@ print(r) # range(0, 10)
 print(type(r)) # range
 
 for i in range(5):
-    print(i, end='|')
-
+    print(i, end='|') # 0|1|2|3|4
 print()
 
 r = range(20, 5, -5)
-print(r[0:2]) # range(20, 10, -5)
+rr = range(5, 20, 5) # range(5, 20, 5) 
+print(rr[0:2]) # range(5, 15, 5) (up to 15)
+print(r[0:2]) # range(20, 10, -5) [10 is ending point & it's not included]
 print(r[0]) # 20
 print(r[1]) # 15

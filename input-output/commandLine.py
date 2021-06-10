e9619cfb8c8f75a0443bced34ed1ac5fee15fbea
Imran Rashid <75940593+imran-rashid@users.noreply.github.com>
2021-06-10 09:33:31 +0600

    Update commandLine.py

--- input-output/commandLine.py
+++ input-output/commandLine.py
@@ -1,20 +1,19 @@
 from sys import argv
 
-print('The number of cmd line args', len(argv))
-print('The line of cmd line args', argv)
-print('Print all them')
-argv.remove(argv[0]) # it removes first index value
+print('The number of cmd line args', len(argv)) # be default len is 1(file_name)
+print(type(argv)) # list
+print('The list of cmd line args', argv) # ['...']
+print('Print all values')
+argv.remove(argv[0]) # it removes first index value which is file_name
 
-[print(i) for i in argv]
+for i in argv: 
+    print(i)
 
 # program to print sum of given numbers provided as cmd line args
-argv.remove(argv[0])
-if argv == []:
-    print('The sum is', 0)
-else:
-    print('The sum is', sum((int(i) for i in argv)))
+print('The sum is ', 0) if argv == [] else print('The sum is', sum((int(i) for i in argv)))
 
 # space is the separator between command line args. All args are str by default
-# long space separator string should be enclosed by "" [python file.py "file x.txt"] otherwise it will consider each as different string
-# if we put '', it will return only 'file [python file.py 'file x.txt']
+# long space separator string should be enclosed by "" (python file.py "Leya Falcon") otherwise it will consider each as different string
+# if we put '', it will return (argv[1]) only 'file_' (python file.py 'file_ x.txt') [ takes space as separatoor]
 
+# print(argv[100]) => try to print out of range, it will return Indexerror: out of range

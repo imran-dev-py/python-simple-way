bb0ef12a8f63834050a5332596c842161bd3718c
Imran Rashid <75940593+imran-rashid@users.noreply.github.com>
2021-06-10 10:59:55 +0600

    Update formatted_str.py

--- input-output/formatted_str.py
+++ input-output/formatted_str.py
@@ -11,10 +11,9 @@ print('a is %d, b is %f, c is %s' % (a, b, c))
 
 name = 'Durga'
 marks = tuple(range(10, 50, 10))
-
-print("Hello %s, your marks are %s" % (name,marks))
+print("Hello %s, your marks are %s" % (name,marks)) # Hello Durga, your marks are (10, 20, 30, 40)
 
 # price casting
 price = 70.56784
-print('Price value is {:0.2f}'.format(price))
-print("Price value is %.2f" %price)
+print('Price value is {:0.2f}'.format(price)) # 70.57
+print("Price value is %.2f" %price) # 70.57

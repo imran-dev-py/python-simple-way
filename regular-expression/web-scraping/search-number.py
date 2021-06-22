1900e0fa459a05dfa5c6f7c7cc71f446c10b4f40
Imran Rashid <75940593+imran-rashid@users.noreply.github.com>
2021-06-22 16:34:18 +0600

    Update search-number.py

--- regular-expression/web-scraping/search-number.py
+++ regular-expression/web-scraping/search-number.py
@@ -5,9 +5,9 @@ import urllib.request
 
 openUrl = urllib.request.urlopen(
     "https://www.redbus.in/info/contactus")
-readUrl = openUrl.read()
+readUrl = str(openUrl.read())
 
 phoneNumbers = re.findall(
-    r'[+][1-9]{4}\s[0-9]{4}\s[0-9]{2}', str(readUrl))
+    r'[+]91[6-9]\d{9}', readUrl)
 
 print(phoneNumbers)
